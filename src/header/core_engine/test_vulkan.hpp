a533f404b00d5d3195b52dd3ffd128f2484085ed
hylmithecoder <hylmimuhammadfiarymahdi@gmail.com>
2025-05-20 07:13:26 +0700

    Minor Update

--- src/header/core_engine/test_vulkan.hpp
+++ src/header/core_engine/test_vulkan.hpp
@@ -1,7 +1,20 @@
 #pragma once
 
+#define VK_USE_PLATFORM_WIN32_KHR
 #include <SDL.h>
 #include <vulkan/vulkan.h>
+#include <vector>
+#include <optional>
+#include <string>
+
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
+    
+    bool isComplete() {
+        return graphicsFamily.has_value() && presentFamily.has_value();
+    }
+};
 
 class TestVulkan {
 public:
@@ -24,6 +37,14 @@ private:
     VkCommandPool commandPool = VK_NULL_HANDLE;
     VkDescriptorPool descriptorPool = VK_NULL_HANDLE;
 
+    VkSwapchainKHR swapchain;
+    std::vector<VkImage> swapchainImages;
+    std::vector<VkImageView> swapchainImageViews;
+    VkFormat swapchainImageFormat;
+    VkExtent2D swapchainExtent;
+    VkRenderPass renderPass;
+    std::vector<VkFramebuffer> swapchainFramebuffers;
+
     // Queue family indices
     int graphicsQueueFamily = -1;
     int presentQueueFamily = -1;
@@ -36,8 +57,30 @@ private:
     void createCommandPool();
     void createDescriptorPool();
     void setupImGui();
+    void createSwapChain();
+    void createRenderPass();
 
     // Helper functions
     VkCommandBuffer beginSingleTimeCommands();
     void endSingleTimeCommands(VkCommandBuffer commandBuffer);
+
+     // Add these new members
+    const std::vector<const char*> deviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+    
+    // Add these new helper functions
+    bool checkValidationLayerSupport();
+    std::vector<const char*> getRequiredExtensions();
+    bool isDeviceSuitable(VkPhysicalDevice device);
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+    bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+    
+    // Debug messenger
+    VkDebugUtilsMessengerEXT debugMessenger = VK_NULL_HANDLE;
+    void setupDebugMessenger();
+
+    int ratePhysicalDevice(VkPhysicalDevice device);
+    void printDeviceProperties(VkPhysicalDevice device);
+    bool isDiscreteGPU(VkPhysicalDevice device);
 };
--- src/scripts/test_vulkan.cpp
+++ src/scripts/test_vulkan.cpp
@@ -9,12 +9,19 @@
 #include <imgui_impl_vulkan.h>
 #include <SDL_ttf.h>
 #include <windows.h>
+#include <map>
+#include <iomanip>
 
 TestVulkan::TestVulkan(SDL_Window* window) : sdlWindow(window) {
     createInstance();
     createSurface();
     pickPhysicalDevice();
     createLogicalDevice();
+    createSwapChain();
+    createRenderPass();
+    // ratePhysicalDevice(physicalDevice);
+    // printDeviceProperties(physicalDevice);
+    // isDiscreteGPU(physicalDevice);
     createCommandPool();
     createDescriptorPool();
     setupImGui();
@@ -44,7 +51,7 @@ void TestVulkan::createInstance() {
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "IlmeeEngine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_1;  // Updated to 1.1 for better compatibility
+    appInfo.apiVersion = VK_API_VERSION_1_3;  // Updated to 1.1 for better compatibility
 
     // Get required extensions from SDL
     unsigned int extensionCount = 0;
@@ -87,48 +94,48 @@ void TestVulkan::createSurface() {
     }
 }
 
-void TestVulkan::pickPhysicalDevice() {
-    uint32_t deviceCount = 0;
-    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+// void TestVulkan::pickPhysicalDevice() {
+//     uint32_t deviceCount = 0;
+//     vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
     
-    if (deviceCount == 0) {
-        throw std::runtime_error("Failed to find GPUs with Vulkan support!");
-    }
+//     if (deviceCount == 0) {
+//         throw std::runtime_error("Failed to find GPUs with Vulkan support!");
+//     }
     
-    std::vector<VkPhysicalDevice> devices(deviceCount);
-    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+//     std::vector<VkPhysicalDevice> devices(deviceCount);
+//     vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
     
-    // Just pick the first device for simplicity
-    physicalDevice = devices[0];
+//     // Just pick the first device for simplicity
+//     physicalDevice = devices[0];
     
-    // Get queue family indices
-    uint32_t queueFamilyCount = 0;
-    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, nullptr);
-    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
-    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, queueFamilies.data());
+//     // Get queue family indices
+//     uint32_t queueFamilyCount = 0;
+//     vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, nullptr);
+//     std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+//     vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, queueFamilies.data());
     
-    // Find graphics queue family index
-    for (uint32_t i = 0; i < queueFamilyCount; i++) {
-        if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-            graphicsQueueFamily = i;
-            break;
-        }
-    }
+//     // Find graphics queue family index
+//     for (uint32_t i = 0; i < queueFamilyCount; i++) {
+//         if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+//             graphicsQueueFamily = i;
+//             break;
+//         }
+//     }
     
-    // Find presentation queue family index
-    for (uint32_t i = 0; i < queueFamilyCount; i++) {
-        VkBool32 presentSupport = false;
-        vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevice, i, surface, &presentSupport);
-        if (presentSupport) {
-            presentQueueFamily = i;
-            break;
-        }
-    }
+//     // Find presentation queue family index
+//     for (uint32_t i = 0; i < queueFamilyCount; i++) {
+//         VkBool32 presentSupport = false;
+//         vkGetPhysicalDeviceSurfaceSupportKHR(physicalDevice, i, surface, &presentSupport);
+//         if (presentSupport) {
+//             presentQueueFamily = i;
+//             break;
+//         }
+//     }
     
-    if (graphicsQueueFamily < 0 || presentQueueFamily < 0) {
-        throw std::runtime_error("Failed to find suitable queue families!");
-    }
-}
+//     if (graphicsQueueFamily < 0 || presentQueueFamily < 0) {
+//         throw std::runtime_error("Failed to find suitable queue families!");
+//     }
+// }
 
 void TestVulkan::createLogicalDevice() {
     // Create a set of unique queue families needed
@@ -229,6 +236,12 @@ void TestVulkan::setupImGui() {
     
     // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForVulkan(sdlWindow);
+
+    // Retrieve swapchain images before using their count
+    uint32_t swapchainImageCount = 0;
+    vkGetSwapchainImagesKHR(device, swapchain, &swapchainImageCount, nullptr);
+    std::vector<VkImage> swapchainImages(swapchainImageCount);
+    vkGetSwapchainImagesKHR(device, swapchain, &swapchainImageCount, swapchainImages.data());
     
     // Initialize ImGui for Vulkan
     ImGui_ImplVulkan_InitInfo initInfo{};
@@ -240,19 +253,309 @@ void TestVulkan::setupImGui() {
     initInfo.PipelineCache = VK_NULL_HANDLE;
     initInfo.DescriptorPool = descriptorPool;
     initInfo.MinImageCount = 2;
-    initInfo.ImageCount = 2;
+    initInfo.ImageCount = swapchainImages.size();
     initInfo.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+    initInfo.RenderPass = renderPass;  // Add this line
     
-    // This is simplified - in a real application you'd need to create
-    // render pass, framebuffers, swapchain, etc.
-    // For demo purposes we're not rendering to screen yet
-    ImGui_ImplVulkan_Init(&initInfo);
-    
+    if (!ImGui_ImplVulkan_Init(&initInfo)) {
+        throw std::runtime_error("Failed to initialize ImGui Vulkan implementation");
+    }
+
     // Upload fonts
     VkCommandBuffer commandBuffer = beginSingleTimeCommands();
     ImGui_ImplVulkan_CreateFontsTexture();
     endSingleTimeCommands(commandBuffer);
-    // ImGui_ImplVulkan_DestroyFontUploadObjects();
+    // ImGui_ImplVulkan_DestroyFontUploadObjects();    
+}
+
+void TestVulkan::createRenderPass() {
+    VkAttachmentDescription colorAttachment{};
+    colorAttachment.format = swapchainImageFormat;
+    colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference colorAttachmentRef{};
+    colorAttachmentRef.attachment = 0;
+    colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass{};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &colorAttachmentRef;
+
+    VkRenderPassCreateInfo renderPassInfo{};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    renderPassInfo.attachmentCount = 1;
+    renderPassInfo.pAttachments = &colorAttachment;
+    renderPassInfo.subpassCount = 1;
+    renderPassInfo.pSubpasses = &subpass;
+
+    if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create render pass!");
+    }
+}
+
+void TestVulkan::createSwapChain() {
+    // Get surface capabilities
+    VkSurfaceCapabilitiesKHR capabilities;
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &capabilities);
+
+    // Choose surface format
+    uint32_t formatCount;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount, nullptr);
+    std::vector<VkSurfaceFormatKHR> formats(formatCount);
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount, formats.data());
+    
+    swapchainImageFormat = formats[0].format;
+    VkColorSpaceKHR colorSpace = formats[0].colorSpace;
+
+    // Create swapchain
+    VkSwapchainCreateInfoKHR createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    createInfo.surface = surface;
+    createInfo.minImageCount = capabilities.minImageCount + 1;
+    createInfo.imageFormat = swapchainImageFormat;
+    createInfo.imageColorSpace = colorSpace;
+    createInfo.imageExtent = capabilities.currentExtent;
+    createInfo.imageArrayLayers = 1;
+    createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    createInfo.preTransform = capabilities.currentTransform;
+    createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    createInfo.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+    createInfo.clipped = VK_TRUE;
+
+    if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapchain) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create swap chain!");
+    }
+}
+
+void TestVulkan::pickPhysicalDevice() {
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+    
+    if (deviceCount == 0) {
+        throw std::runtime_error("Failed to find GPUs with Vulkan support!");
+    }
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    // Rate devices and pick the best one
+    std::multimap<int, VkPhysicalDevice> candidates;
+    
+    for (const auto& device : devices) {
+        int score = ratePhysicalDevice(device);
+        candidates.insert(std::make_pair(score, device));
+        printDeviceProperties(device);
+    }
+
+    // Check if the best candidate is suitable
+    if (candidates.rbegin()->first > 0) {
+        physicalDevice = candidates.rbegin()->second;
+        VkPhysicalDeviceProperties deviceProps;
+        vkGetPhysicalDeviceProperties(physicalDevice, &deviceProps);
+        std::cout << "Selected GPU: " << deviceProps.deviceName << std::endl;
+    } else {
+        throw std::runtime_error("Failed to find a suitable GPU!");
+    }
+    std::cout << "Using GPU: " << physicalDevice << std::endl;
+}
+
+int TestVulkan::ratePhysicalDevice(VkPhysicalDevice device) {
+    VkPhysicalDeviceProperties deviceProps;
+    VkPhysicalDeviceFeatures deviceFeatures;
+    vkGetPhysicalDeviceProperties(device, &deviceProps);
+    vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+    // Print detailed device info for debugging
+    std::cout << "\nEvaluating device: " << deviceProps.deviceName << std::endl;
+    std::cout << "Device ID: " << deviceProps.deviceID << std::endl;
+    std::cout << "Vendor ID: " << deviceProps.vendorID << std::endl;
+    std::cout << "Driver Version: " << deviceProps.driverVersion << std::endl;
+
+    int score = 0;
+
+    // Check for compute capabilities
+    VkPhysicalDeviceMemoryProperties memProps;
+    vkGetPhysicalDeviceMemoryProperties(device, &memProps);
+
+    // Check queue families for compute support
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    bool hasComputeSupport = false;
+    for (const auto& queueFamily : queueFamilies) {
+        if (queueFamily.queueFlags & VK_QUEUE_COMPUTE_BIT) {
+            hasComputeSupport = true;
+            score += 500; // Bonus for compute support
+            std::cout << "Device has compute support" << std::endl;
+            break;
+        }
+    }
+
+    // Score based on device type
+    switch(deviceProps.deviceType) {
+        case VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU:
+            score += 1000;
+            std::cout << "Discrete GPU: +1000 points" << std::endl;
+            break;
+        case VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU:
+            score += 100;
+            std::cout << "Integrated GPU: +100 points" << std::endl;
+            break;
+        case VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU:
+            score += 50;
+            std::cout << "Virtual GPU: +50 points" << std::endl;
+            break;
+        default:
+            std::cout << "Other GPU type: +0 points" << std::endl;
+            break;
+    }
+
+    // Score based on memory size
+    VkDeviceSize maxHeapSize = 0;
+    for(uint32_t i = 0; i < memProps.memoryHeapCount; i++) {
+        if(memProps.memoryHeaps[i].flags & VK_MEMORY_HEAP_DEVICE_LOCAL_BIT) {
+            maxHeapSize = std::max(maxHeapSize, memProps.memoryHeaps[i].size);
+        }
+    }
+    
+    // Convert to GB and add to score
+    score += static_cast<int>(maxHeapSize / (1024 * 1024 * 1024));
+    std::cout << "Memory size score: +" << (maxHeapSize / (1024 * 1024 * 1024)) << " points" << std::endl;
+
+    // Check for specific features
+    if (deviceFeatures.geometryShader) {
+        score += 100;
+        std::cout << "Has geometry shader: +100 points" << std::endl;
+    }
+    if (deviceFeatures.tessellationShader) {
+        score += 100;
+        std::cout << "Has tessellation: +100 points" << std::endl;
+    }
+
+    std::cout << "Final score: " << score << std::endl;
+    std::cout << "------------------------" << std::endl;
+
+    return score;
+}
+
+bool TestVulkan::isDiscreteGPU(VkPhysicalDevice device) {
+    VkPhysicalDeviceProperties deviceProps;
+    vkGetPhysicalDeviceProperties(device, &deviceProps);
+    return deviceProps.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU;
+}
+
+QueueFamilyIndices TestVulkan::findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies) {
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            indices.graphicsFamily = i;
+        }
+
+        VkBool32 presentSupport = false;
+        vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+        if (presentSupport) {
+            indices.presentFamily = i;
+        }
+
+        if (indices.isComplete()) {
+            break;
+        }
+
+        i++;
+    }
+
+    return indices;
+}
+
+void TestVulkan::printDeviceProperties(VkPhysicalDevice device) {
+    VkPhysicalDeviceProperties deviceProps;
+    VkPhysicalDeviceMemoryProperties memProps;
+    VkPhysicalDeviceFeatures deviceFeatures;
+    
+    vkGetPhysicalDeviceProperties(device, &deviceProps);
+    vkGetPhysicalDeviceMemoryProperties(device, &memProps);
+    vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+    std::cout << "\n=== Device Details ===" << std::endl;
+    std::cout << "Device Name: " << deviceProps.deviceName << std::endl;
+    std::cout << "Device ID: 0x" << std::hex << deviceProps.deviceID << std::dec << std::endl;
+    std::cout << "Vendor ID: 0x" << std::hex << deviceProps.vendorID << std::dec << std::endl;
+    
+    std::cout << "Device Type: ";
+    switch(deviceProps.deviceType) {
+        case VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU:
+            std::cout << "Integrated GPU";
+            break;
+        case VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU:
+            std::cout << "Discrete GPU";
+            break;
+        case VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU:
+            std::cout << "Virtual GPU";
+            break;
+        case VK_PHYSICAL_DEVICE_TYPE_CPU:
+            std::cout << "CPU";
+            break;
+        default:
+            std::cout << "Other";
+    }
+    std::cout << std::endl;
+
+    // Print memory information
+    std::cout << "\nMemory Heaps:" << std::endl;
+    for(uint32_t i = 0; i < memProps.memoryHeapCount; i++) {
+        float sizeGB = static_cast<float>(memProps.memoryHeaps[i].size) / (1024.0f * 1024.0f * 1024.0f);
+        std::cout << "  Heap " << i << ": " << std::fixed << std::setprecision(2) << sizeGB << " GB";
+        if(memProps.memoryHeaps[i].flags & VK_MEMORY_HEAP_DEVICE_LOCAL_BIT)
+            std::cout << " (Device Local)";
+        std::cout << std::endl;
+    }
+
+    // Print queue families
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    std::cout << "\nQueue Families:" << std::endl;
+    for(uint32_t i = 0; i < queueFamilyCount; i++) {
+        std::cout << "  Family " << i << ":" << std::endl;
+        std::cout << "    Queue Count: " << queueFamilies[i].queueCount << std::endl;
+        std::cout << "    Capabilities:";
+        if(queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            std::cout << " Graphics";
+        if(queueFamilies[i].queueFlags & VK_QUEUE_COMPUTE_BIT)
+            std::cout << " Compute";
+        if(queueFamilies[i].queueFlags & VK_QUEUE_TRANSFER_BIT)
+            std::cout << " Transfer";
+        std::cout << std::endl;
+    }
+
+    std::cout << "\nAPI Version: " 
+              << VK_VERSION_MAJOR(deviceProps.apiVersion) << "."
+              << VK_VERSION_MINOR(deviceProps.apiVersion) << "."
+              << VK_VERSION_PATCH(deviceProps.apiVersion) << std::endl;
+    std::cout << "Driver Version: "
+              << VK_VERSION_MAJOR(deviceProps.driverVersion) << "."
+              << VK_VERSION_MINOR(deviceProps.driverVersion) << "."
+              << VK_VERSION_PATCH(deviceProps.driverVersion) << std::endl;
+    std::cout << "========================" << std::endl;
 }
 
 VkCommandBuffer TestVulkan::beginSingleTimeCommands() {
